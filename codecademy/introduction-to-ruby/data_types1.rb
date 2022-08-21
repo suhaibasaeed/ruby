5733dab0e8c39483304691f19af59d419401988c
Suhaib Saeed <saeed.suhaib@googlemail.com>
2022-08-21 23:33:41 +0100

    Added first file

---
+++ codecademy/introduction-to-ruby/data_types1.rb
@@ -0,0 +1,10 @@
+# Numeric type
+my_num = 25     # Add your code here!
+# Boolean type
+my_boolean = true    # And here!
+# String type
+my_string =  "Ruby"   # Also here.
+# Print statements
+puts my_num
+puts my_boolean
+puts my_string
